afb7578b9960bdb8b47486a13beed5c0182dad49
Raker03 <pitohuibg2k3@gmail.com>
2021-09-14 22:21:45 +0700

    12

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+//console.log("hello world");
+//var name = " Duc";
+//var array = [1, 2, "a"];
+//var mutiArray = [
+//    [1, 2, 3],
+//    [5, 6, 7]
+//];
+//var object = {}
+document.getElementById('clickme').onclick = function() {
+    alert('bạn sẽ chết sau 1 tuần');
+};
